0942bc1ca5b8d83734108ba6709b3a0d17d1e0b8
Kevin Pena <kevinpena160@gmail.com>
2018-05-06 14:20:47 -0500

    working time

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -16,25 +16,39 @@ var query = database.ref().orderByKey();
 query.once("value").then(function (snapshot) {
     snapshot.forEach(function (childSnapshot) {
 
+        var now = moment();
         var newRow = $("<tr>");
         var key = childSnapshot.key;
         var childData = childSnapshot.val();
-        console.log(childData);
-        var column = $("<td>");
+        column = $("<td>");
         column.text(childData.name);
         newRow.append(column);
-        var column = $("<td>");
+        column = $("<td>");
         column.text(childData.destination);
         newRow.append(column);
-        var column = $("<td>");
+        column = $("<td>");
         column.text(childData.frequency);
         newRow.append(column);
-        var column = $("<td>");
-        column.text(childData.firstTrainTime);
+        column = $("<td>");
+
+        var start = moment((childData.firstTrainTime), "HH:mm");
+        var end = moment();
+        if (start.isBefore(end)) start.add(childData.frequency, 'minutes');
+        
+        column.text(moment(start).format("HH:mm"));
         newRow.append(column);
-        var column = $("<td>");
-        column.text(childData.minutesAway);
+        column = $("<td>");
+
+        var d = moment.duration(start.diff(end));
+        var minutes = (d.minutes() + 1);
+        var hour = d.hours();
+        minutes = minutes + (hour * 60)
+        column.text(minutes);
         newRow.append(column);
+
+        database.ref(key).update({
+
+            minutesAway: minutes});
 
         $("#tableBody").append(newRow);
 
@@ -69,33 +83,23 @@ $("#addButton").on("click", function (event) {
     });
 
     var ftt = trainObj.firstTrain;
-
-    console.log(trainObj.name);
-    console.log(trainObj.destination);
-
     var column = $("<td>");
+
     column.text(ftt);
     newRow.append(column);
 
 
     var start = moment(ftt, "HH:mm");
     var end = moment();
-    console.log(start)
-    console.log(end)
 
     // account for crossing over to midnight the next day
     if (start.isBefore(end)) start.add(1, 'day');
 
     var d = moment.duration(start.diff(end));
-    console.log(d);
-
     var minutes = (d.minutes() + 1);
     var hour = d.hours();
     minutes = minutes + (hour * 60)
-
-    console.log(minutes);
-    console.log(hour);
-
+    
     var column = $("<td>");
     column.text(minutes);
     newRow.append(column);
@@ -120,5 +124,3 @@ $("#addButton").on("click", function (event) {
 //     var ran = moment.duration(ftt.diff(today))
 //     console.log(ftt)
 // }
-
-
